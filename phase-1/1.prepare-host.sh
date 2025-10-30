fb38054c7fba6e3717e896aa5f558cec92174de7
Jeff He <jeff.he@outlook.com>
2025-10-30 11:19:29 +0800

    phase-1 partly done.
    Signed-off-by: Jeff He <jeff.he@outlook.com>

--- phase-1/1.prepare-host.sh
+++ phase-1/1.prepare-host.sh
@@ -8,13 +8,18 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 
 # Install Docker Compose v2
-sudo apt install -y docker-compose-plugin
+# if you installed docker from docker official site, just run 'sudo apt install -y docker-compose-plugin docker-buildx-plugin'
+# else you still want to use docker from debian release, manually install docker compose v2 and docker buildx plugins following the instructions at https://github.com/docker/{compose, buildx} 
 
 # Verify and enable KVM for Android emulation
 sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 sudo adduser $USER kvm
 sudo adduser $USER libvirt
-kvm-ok  # Should return "KVM acceleration can be used"
+sudo kvm-ok  # Should return "KVM acceleration can be used"
 
 # Install Android emulator dependencies
 sudo apt install -y openjdk-17-jdk adb
+
+# activate libvirtd
+sudo systemctl enable libvirtd
+sudo systemctl status libvirtd
